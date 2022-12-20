3e50bc9cfb3ca869b292864ecceeb3b951f92b2e
ilya946 <58272745+ilya946@users.noreply.github.com>
2022-12-21 01:04:49 +0300

    MyProject

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class Program
+    {
+        public static long factorial(int fact)
+        {
+            int h1 = fact;
+            long h2 = 1;
+            if (fact == 0)
+                return 1;
+            else
+            {
+                for (int i = 1; i <= h1; i++)
+                {
+                    h2 *= i;
+                }
+            }
+            return h2;
+        }
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine( factorial(6));
+        }
+    }
+}
+
